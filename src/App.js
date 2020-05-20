c4fca0aa76b3eb923c5fa3eddbe66aefcfaa85f3
SoraJung <jarasasa2@gmail.com>
2020-05-21 00:25:10 +0900

    resuable components with jsx + props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+function Food({ favorite }) {
+  //console.log(props.fav);
+  //props.fav 와 { fav }는 같다 
+  //function Food(props)
+  //<h1>I like {props.fav}</h1>;
+  return <h1>I like {favorite}</h1>;
+}
+
 function App() {
   return (
     <div>
       <h1>Hello</h1>
+      <Food favorite ="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samgiopsal" />
+      <Food favorite="chukumi" />
     </div>
   );
 }
